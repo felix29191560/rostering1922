bcc6a4c29e9bc40ba7b207c945666ff9052db93d
felix29191560 <cato_cyli@hotmail.com>
2025-09-07 11:13:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
 // Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCe4HzQ3FXadF_drpR7XOyVQYIYi36L8KM",
@@ -9,8 +7,6 @@ const firebaseConfig = {
   messagingSenderId: "853988161908",
   appId: "1:853988161908:web:ae269331b50b59dd271421"
 };
-
-const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 let db;
